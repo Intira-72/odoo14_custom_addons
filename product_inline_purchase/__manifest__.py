a30f1b8fa7e4eabf186a0bd65791142eb88b44fb
H.FoxDev <intira.vinit@gmail.com>
2023-01-03 16:50:54 +0700

    fix bug purduct_inline_purchase
    - Fixed a bug with retrieving the purchase price from the seller in the purchase requisition creation process.

--- product_inline_purchase/__manifest__.py
+++ product_inline_purchase/__manifest__.py
@@ -2,10 +2,10 @@
 {
     'name': "Product In Line (Purchase)",
     'summary': """Added display of product names on the purchase order list.""",
-    'description': """""",
+    'description': """Fixed a bug with retrieving the purchase price from the seller in the purchase requisition creation process.""",
     'author': "My Company",
     'category': 'purchase',
-    'version': '0.1',
+    'version': '0.2',
     'depends': ['base','purchase'],
     'data': [
         # 'security/ir.model.access.csv',
