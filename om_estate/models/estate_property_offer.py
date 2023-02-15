c7c7bcd6eb6770333d16699366570a042aa4f528
H.FoxDev <intira.vinit@gmail.com>
2023-02-15 11:09:36 +0700

    Stat Buttons

--- om_estate/models/estate_property_offer.py
+++ om_estate/models/estate_property_offer.py
@@ -13,8 +13,10 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=True)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    property_type_id = fields.Many2one(related='property_id.property_type_id', store=True)
     validity = fields.Integer(string="Validity (days)", default=7)
     date_deadline = fields.Date(string="Deadline", compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+    active = fields.Boolean(default=True)
 
     @api.depends('validity')
     def _compute_date_deadline(self):
--- om_estate/models/estate_property_type.py
+++ om_estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class EstatePropertyType(models.Model):
@@ -8,4 +8,12 @@ class EstatePropertyType(models.Model):
 
     name = fields.Char(required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id')
+    offer_ids = fields.One2many('estate.property.offer', 'property_type_id')
+    offer_count = fields.Integer(compute='_compute_offer_count')
     sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
+    
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for rec in self:
+            rec.offer_count = len([offer for offer in rec.offer_ids])
+    
