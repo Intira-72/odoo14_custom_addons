ca7f9ba8d9cbeda41162934f2204039371b80f61
Intira-72 <intira.vinit@gmail.com>
2023-03-11 09:12:01 +0700

    Update estate_inherited_res_users.py

--- om_estate/models/estate_inherited_res_users.py
+++ om_estate/models/estate_inherited_res_users.py
@@ -4,8 +4,4 @@ class Users(models.Model):
     _inherit = 'res.users'
 
     property_ids = fields.One2many('estate.property', 'user_id', default=None)
-
-    @api.onchange('name')
-    def _onchange_(self):
-        print(self.property_ids)
-        
+    
