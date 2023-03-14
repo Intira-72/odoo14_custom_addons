7c1368c0c77e4c40b4389a0c7710fbaac6097fc8
Intira-72 <intira.vinit@gmail.com>
2023-03-14 16:45:28 +0700

    create all invoices by sale orders

--- om_custom_invoice_print/__manifest__.py
+++ om_custom_invoice_print/__manifest__.py
@@ -4,21 +4,20 @@
     'summary': """Modify invoice printing format""",
     'description': """Modify invoice printing format""",
     'author': "",
-    # 'website': "http://www.yourcompany.com",
     'category': 'Sales',
     'version': '0.1',
     'depends': ['base', 'sale'],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/template.xml',
         'views/account_move_inher.xml',
+        'wizard/create_invoice_wiz.xml',
         'report/report.xml',
         'report/custom_invoice.xml',
     ],
     # only loaded in demonstration mode
     'demo': [],
     'qweb': [
-        # "static/src/xml/btn_shipping_report.xml",
     ],
     'installable': True,
     'application': True,
---
+++ om_custom_invoice_print/wizard/create_invoice_wiz.py
@@ -0,0 +1,33 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+class MultipleInvoiceCreate(models.TransientModel):
+    _name = "multiple.invoice.create"
+    _description = "Create Invoice By SO"
+
+    def create_invoice_by_order(self):
+        for record in self._context.get('active_ids'):            
+            sale_order = self.env[self._context.get('active_model')].browse(record)
+            order_line = self.env['sale.order.line'].search([('order_id', '=', record)])
+            if sale_order.invoice_status == 'to invoice':
+                context = {
+                    'partner_id': sale_order.partner_invoice_id,
+                    'partner_shipping_id': sale_order.partner_shipping_id,
+                    'invoice_payment_term_id': sale_order.payment_term_id,
+                    'move_type': 'out_invoice',
+                    'invoice_origin': sale_order.name,
+                    'invoice_line_ids': [(0, 0,
+                        {'product_id': i.product_id,
+                         'quantity': i.qty_to_invoice,
+                         'product_uom_id': i.product_uom,
+                         'price_unit': i.price_unit,
+                         'tax_ids': i.tax_id,
+                         }) for i in order_line
+                    ]
+                }
+
+                self.env['account.move'].create(context)
+            sale_order.write({'invoice_status': 'invoiced'})
+            
+        return True
+            
